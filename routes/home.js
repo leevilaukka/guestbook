e89ca1b3b7b382468c4830e644679d7ca48192a5
Leevi Laukka <leevi.laukka@gmail.com>
2022-04-01 17:54:45 +0300

    fixes

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index", {title: "Moi"});
+    res.render("index", {title: "Guestbook"});
 });
 
 module.exports = router;
